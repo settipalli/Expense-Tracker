0078dbce2a6dfefc48ffd8d125c58bc60676087f
Santhosh Settipalli <santhosh.settipalli@skyscanner.net>
2019-02-11 00:51:02 +0000

    Added Project title and Add Expense card.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="row title-border">
+                    <div className="col">
+                        <h1 className="bd-title">Expense Tracker</h1>
+                    </div>
+                </div>
+                <div className="container container-body">
+                    <div className="row">
+                        <div className="col-sm">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Add Expense</h5>
+                                    <hr/>
+                                    <p className="card-text">
+                                        <form>
+                                            <input type="text" className="form-control form-control-lg"
+                                                   placeholder="Amount" />
+                                        </form>
+                                    </p>
+                                    <a href="/" className="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-      </div>
-    )
-  }
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App
