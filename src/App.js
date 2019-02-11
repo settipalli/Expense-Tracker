2c4cb6a1b535d53da88766585f95840760d758e5
Santhosh Settipalli <santhosh.settipalli@skyscanner.net>
2019-02-11 16:11:25 +0000

    Add Expense Card.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {MapPin} from 'react-feather'
 import './App.css'
 
 class App extends Component {
@@ -15,15 +16,30 @@ class App extends Component {
                         <div className="col-sm">
                             <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title">Add Expense</h5>
+                                    <div className="row justify-content-between">
+                                        <div className="col">
+                                            <h5 className="card-title">Add Expense</h5>
+                                        </div>
+                                        <div className="col-3-auto">
+                                            <div className="row">
+                                                <div className="col-1">
+                                                    <MapPin color="#999" size="20"/>
+                                                </div>
+                                                <div className="col">
+                                                    <span>London, UK</span>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
                                     <hr/>
                                     <p className="card-text">
                                         <form>
                                             <input type="text" className="form-control form-control-lg"
-                                                   placeholder="Amount" />
+                                                   placeholder="Amount"/>
                                         </form>
                                     </p>
-                                    <a href="/" className="btn btn-primary">Go somewhere</a>
+                                    <a href="/" className="btn btn-lg btn-primary">Save</a>
                                 </div>
                             </div>
                         </div>
